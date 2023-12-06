28f1bc0a625e826134cca2c5ecac381fba58e73d
Tochiepet <alexpeter215@gmail.com>
2023-12-07 00:36:23 +0100

    most tasks

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,2 +1,5 @@
-#!/use/bin/python3
-
+#!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+    return (a_dictionary)
